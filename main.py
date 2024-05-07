2b699c4ed7d74c510b36d804052d24c19f2379f8
kkadenw <kadenwu925.201@gmail.com>
2024-05-07 11:00:10 -0400

    Started code
    Started to add code to background and  pygame. ( Might change background to be a GIF )

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import pygame
 import random
+pygame.init()
+
+r = 50
+g = 0
+b = 100
+
+#window dimensions
+window_width = 1274
+window_height = 980
+window_size = ( window_width, window_height)
+bg_display = pygame.display.set_mode(window_size)
+bg_image = pygame.image.load('CASINO.jpg')
+
+# MAIN LOOP
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    bg_display.blit(bg_image,(0,0))
+    pygame.display.update()
+
+pygame.quit()
+
+
+
+
+
+pygame.quit()
