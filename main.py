6841fad253cb778e60e8bab7c19663df2d94a594
kkadenw <kadenwu925.201@gmail.com>
2024-05-16 10:57:01 -0400

    Character Selection

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import random
 # set up pygame modules
 pygame.init()
 pygame.font.init()
-my_font = pygame.font.SysFont('Arial', 20)
+font = pygame.font.SysFont('Arial', 20)
 pygame.display.set_caption("TROLL CASINO!")
 button_rect = pygame.Rect(125, 125, 150, 50)
-
+space_pressed = False
 # set up variables for the display
+
 size = (1274, 980)
 screen = pygame.display.set_mode(size)
 
 bg = pygame.image.load("CASINO.jpg")
-start = pygame.image.load("Start button.png")
-start = pygame.transform.scale(start, (270, 90))
+space_rendered = font.render("Press SPACE to Enter The Casino!", True, (255, 255, 255))
 
 lst = []
 # render the text for later
@@ -25,6 +25,7 @@ run = True
 # -------- Main Program Loop -----------
 clock = pygame.time.Clock()
 frame = 0
+character_pick = False
 while run:
     # --- Main event loop
 
@@ -33,13 +34,11 @@ while run:
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
             pos = pygame.mouse.get_pos()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x,y = event.pos
-                if start.get_rect().collidepoint(x, y):
-                    print('clicked on image')
+            if event.type == pygame.K_SPACE:
+                character_pick = True
 
     screen.blit(bg, (0, 0))
-    screen.blit(start, (505,40))
+    screen.blit(space_rendered, (525, 200))
     pygame.display.update()
 
 
