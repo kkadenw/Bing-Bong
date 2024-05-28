0a06fbd0cc325428f0cbca5143940c8976975ccf
kkadenw <kadenwu925.201@gmail.com>
2024-05-28 11:00:59 -0400

    Character Selection

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont('Arial', 20)
+font2 = pygame.font.SysFont('Arial', 50)
 pygame.display.set_caption("TROLL CASINO!")
 button_rect = pygame.Rect(125, 125, 150, 50)
 space_pressed = False
@@ -23,7 +24,11 @@ sg_character = pygame.image.load("S Gorilla Character.png")
 bg = pygame.image.load("CASINO.jpg")
 characterp_background = pygame.image.load("CharacterP Background.jpg")
 space_rendered = font.render("Press SPACE to Enter The Casino!", True, (255, 255, 255))
-character_pick = font.render("Press 1 2 3 To select your character!", True, ( 255, 255, 255))
+character_pick_front = font2.render("Press 1 2 3 To select your character!", True, (255, 255, 255))
+character1 = font2.render("1", True,(255,255, 255))
+character2 = font2.render("2", True,(255,255, 255))
+character3 = font2.render("3", True,(255,255, 255))
+
 
 lst = []
 # render the text for later
@@ -34,11 +39,20 @@ run = True
 clock = pygame.time.Clock()
 frame = 0
 character_pick = False
+character_picked_1 = False
+character_picked_2 = False
+character_picked_3 = False
 while run:
     # --- Main event loop
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         character_pick = True
+        if keys[pygame.K_1]:
+            character_picked_1 = True
+        elif keys[pygame.K_2]:
+            character_picked_2 = True
+        elif keys[pygame.K_3]:
+            character_picked_3 = True
 
 
     for event in pygame.event.get():  # User did something
@@ -52,9 +66,13 @@ while run:
             pygame.display.update()
         if character_pick == True:
             screen.blit(characterp_background, (0, 0))
-            screen.blit(g_character, (50, 100))
-            screen.blit(p_character, (300, 100))
-            screen.blit(sg_character,( 600, 100))
+            screen.blit(g_character, (50, 300))
+            screen.blit(p_character, (500, 300))
+            screen.blit(sg_character,( 900,300))
+            screen.blit(character_pick_front, (330, 100))
+            screen.blit(character1, (75, 400))
+            screen.blit(character2, (500, 400))
+            screen.blit(character3, (925 , 400))
             pygame.display.update()
 
 
