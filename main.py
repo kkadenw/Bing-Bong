8e400fba2be628c37fc44a61248647806dc6aea4
kkadenw <kadenwu925.201@gmail.com>
2024-06-03 10:58:05 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ run = True
 # -------- Main Program Loop -----------
 clock = pygame.time.Clock()
 frame = 0
+start_screen = True
 character_pick = False
 character_pick2 = False
 character_picked_1 = False
@@ -49,15 +50,19 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         character_pick = True
-        if keys[pygame.K_1]:
-            character_picked_1 = True
-            character_pick2 = True
-        elif keys[pygame.K_2]:
-            character_picked_2 = True
-            character_pick2 = True
-        elif keys[pygame.K_3]:
-            character_picked_3 = True
-            character_pick2 = True
+        start_screen = False
+    if keys[pygame.K_1]:
+        character_picked_1 = True
+        character_pick2 = True
+        character_pick = False
+    if keys[pygame.K_2]:
+        character_picked_2 = True
+        character_pick2 = True
+        character_pick = False
+    if keys[pygame.K_3]:
+        character_picked_3 = True
+        character_pick2 = True
+        character_pick = False
 
 
     for event in pygame.event.get():  # User did something
@@ -65,7 +70,7 @@ while run:
             run = False
             pos = pygame.mouse.get_pos()
 
-        if character_pick == False:
+        if start_screen == True:
             screen.blit(bg, (0, 0))
             screen.blit(space_rendered, (525, 200))
             pygame.display.update()
@@ -79,12 +84,15 @@ while run:
             screen.blit(character2, (500, 400))
             screen.blit(character3, (925 , 400))
             pygame.display.update()
-        if character_pick2 == True:
+        if character_pick2 == True and character_picked_1 == True:
             screen.blit(characterp_background, (0, 0))
-            screen.blit(pk_table, (700,450))
-            screen.blit(character1, (25, 100))
-            screen.blit(character2, (10000, 10000))
-            screen.blit(character3, (100000, 10000))
+            screen.blit(pk_table, (300,350))
+            screen.blit(g_character, (15, 40))
+            pygame.display.update()
+        if character_pick2 == True and character_picked_2 == True:
+            screen.blit(characterp_background, (0, 0))
+            screen.blit(pk_table, (300, 350))
+            screen.blit(p_character, ( 15, 40))
             pygame.display.update()
 
 
